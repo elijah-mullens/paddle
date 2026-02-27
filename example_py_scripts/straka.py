198c15577719a94a5f0d279e1f3a781c620a0489
grainy/cli <chengcli@umich.edu>
2026-02-27 13:33:11 -0500

    wip

--- example_py_scripts/straka.py
+++ example_py_scripts/straka.py
@@ -30,7 +30,6 @@ block = MeshBlock(op)
 # use cuda if available
 if torch.cuda.is_available() and op.layout().backend() == "nccl":
     device = torch.device(block.device())
-    print("device = ", device)
 else:
     device = torch.device("cpu")
 
