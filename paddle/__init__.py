0578f11036d86f4dc22116dc7e1e0e12898460d6
Cheng Li <cli@dart2>
2026-06-26 20:37:02 -0400

    wip

--- paddle/__init__.py
+++ paddle/__init__.py
@@ -1,22 +1,9 @@
-from pathlib import Path
-
-import kintera
-
 from .setup_profile import setup_profile
 from .write_profile import write_profile
 from .find_init_params import find_init_params
 from .evolve_kinetics import evolve_kinetics
 from .cubed_sphere_remap import remap_cubed_sphere_files
 from .dist import start_dist, close_dist
-
-
-def _add_packaged_kintera_resources() -> None:
-    data_dir = Path(kintera.__file__).parent / "data"
-    if data_dir.is_dir():
-        kintera.add_resource_directory(str(data_dir))
-
-
-_add_packaged_kintera_resources()
 
 __all__ = [
     "start_dist",
